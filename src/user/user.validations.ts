021b1aee81bdc9a6d739febac3e221feca89e978
Peter Kin <kinpeter85@gmail.com>
2022-07-05 13:28:25 +0200

    Add validation messages

--- src/user/user.validations.ts
+++ src/user/user.validations.ts
@@ -1,17 +1,18 @@
 import * as yup from 'yup'
+import { ErrorCode } from '../common/error-codes'
 
 export const PasswordSchema = yup.object({
-  password: yup.string().min(6).required(),
+  password: yup.string().min(6, ErrorCode.INVALID_FORMAT).required(ErrorCode.FIELD_REQUIRED),
 })
 
 export const AuthSchema = yup
   .object({
-    email: yup.string().email().required(),
+    email: yup.string().email(ErrorCode.INVALID_FORMAT).required(ErrorCode.FIELD_REQUIRED),
   })
   .concat(PasswordSchema)
 
 export const UpdatePasswordSchema = yup
   .object({
-    oldPassword: yup.string().min(6).required(),
+    oldPassword: yup.string().min(6, ErrorCode.INVALID_FORMAT).required(ErrorCode.FIELD_REQUIRED),
   })
   .concat(PasswordSchema)
